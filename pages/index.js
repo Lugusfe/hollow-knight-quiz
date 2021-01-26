401c8b98a88f09dacf9521ea2df805fe2c137949
Luís Ferreira <72242940+Lugusfe@users.noreply.github.com>
2021-01-25 21:22:38 -0300

    código da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,60 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+
+export const QuizContainer = styled.section`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage = {db.bg}>
+
+      <QuizContainer>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Hollow Knigth</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>Lorem Ipsum</p>
+          </Widget.Content>
+        </Widget>
+
+
+        <Widget>
+          <Widget.Content>            
+            <h1>Outros Quizes</h1>
+
+            <p>Lorem Ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Footer>
+
+        </Footer>
+
+      </QuizContainer>
+      <GitHubCorner />  
+
+    </QuizBackground>
+  );
 }
